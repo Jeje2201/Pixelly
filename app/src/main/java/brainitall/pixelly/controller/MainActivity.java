90f338694c15b9ca8f017382bc2e60ee19644f37
smittina <38455141+smittina@users.noreply.github.com>
2018-04-30 11:02:00 +0200

    fichier à ignorer

--- app/src/main/java/brainitall/pixelly/controller/MainActivity.java
+++ app/src/main/java/brainitall/pixelly/controller/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import brainitall.pixelly.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
