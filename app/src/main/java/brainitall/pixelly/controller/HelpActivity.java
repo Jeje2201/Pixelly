7076d2bcc8b2a29a04e0c3ae9ad93711274e7542
lofit13 <38454882+lofit13@users.noreply.github.com>
2018-05-13 23:23:27 +0200

    HelpActivity et LevelActivity
    layout et fichier java réalisés pour les activitées aide et niveau. l'activitée aide n'est pas finis ! j'ai rajouté deux images qui sont utilisées dans l'activitée d'aide

--- app/src/main/java/brainitall/pixelly/controller/HelpActivity.java
+++ app/src/main/java/brainitall/pixelly/controller/HelpActivity.java
@@ -2,14 +2,67 @@ package brainitall.pixelly.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import brainitall.pixelly.R;
 
 public class HelpActivity extends AppCompatActivity {
 
+    /**
+     * textView de l'activitée d'aide
+     */
+    private TextView  mTexteAide;
+
+    /**
+     * première image présentée dans l'activitée aide
+     */
+    private ImageView mImage1;
+
+    /**
+     * seconde image présentée dans l'activitée aide
+     */
+
+    private ImageView mImage2;
+
+    private TextView mTexteAide2;
+
+    private Button mBouton;
+
+    private RelativeLayout mRelativeLayout;
+
+    private TextView mTexteAide3;
+
+    private Button mBouton2;
+
+
     @Override
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        // branchement de chaque widget de cette activitée
+        mTexteAide = (TextView) findViewById(R.id.activity_help_textview1); // on réference l'élement graphique mTexteAide(TextView)
+        mImage1 = (ImageView) findViewById(R.id.activity_help_image1); // on réference l'élément graphique mImage1 (ImageView)
+        mImage2= (ImageView) findViewById(R.id.activity_help_image2);
+        mTexteAide2 = ( TextView) findViewById(R.id.activity_help_textview2);
+        mBouton = (Button) findViewById(R.id.activity_help_Button1);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.activity_help_detailLayout);
+        mTexteAide3 = (TextView) findViewById(R.id.activity_help_textview3);
+        mBouton2 = (Button) findViewById(R.id.activity_help_Button2);
+
+        mBouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRelativeLayout.setVisibility(View.VISIBLE); // le clic sur le bouton, rend le relativeLayout visible
+                mBouton.setVisibility(View.GONE); // le bouton " plus de détail" est masqué
+            }
+        });
     }
 }
--- app/src/main/java/brainitall/pixelly/controller/LevelActivity.java
+++ app/src/main/java/brainitall/pixelly/controller/LevelActivity.java
@@ -5,24 +5,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import brainitall.pixelly.R;
 
 public class LevelActivity extends AppCompatActivity {
 
+    /**
+     * bouton niveau test
+     */
     private Button mNiveauTest;
+
+    /**
+     * bouton niveau 1 qui sera bloqué au démarage
+     */
+    private Button mNiveau1;
+
+    /**
+     * boutou niveau 2 qui sera bloqué au démarrage
+     */
+    private Button mNiveau2;
+
+    /**
+     * bouton aide
+     */
+    private Button mHelp;
+
+    /**
+     * texte affiché sur la page de niveau
+     */
+    private TextView mListeNiveaux;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
 
         // Branchements Vue / Controller
-        mNiveauTest = (Button) findViewById(R.id.activity_level_btn_niveauTest);
+        mNiveauTest = (Button) findViewById(R.id.activity_level_btn_niveauTest); //on réference l'element graphique mNiveauTest
+        mNiveau1 = (Button) findViewById(R.id.activity_level_btn_niveau1);
+        mNiveau2 = (Button) findViewById(R.id.activity_level_btn_niveau2);
+        mHelp = (Button) findViewById(R.id.activity_main_btn_aide);
 
         mNiveauTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent playActivity = new Intent(LevelActivity.this, PlayActivity.class);
+                mNiveau1.setEnabled(false); // on désactive le bouton niveau 1
+                mNiveau2.setEnabled(false); // on désactive le bouton niveau 2 qui sera debloqué lors d'un succés au niveau 1
                 startActivity(playActivity);
             }
         });
