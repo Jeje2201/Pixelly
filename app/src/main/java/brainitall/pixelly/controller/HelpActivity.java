ea2f6a75c8a9cc9e60716bbebb1afe66609f11b6
lofit13 <38454882+lofit13@users.noreply.github.com>
2018-05-14 12:05:10 +0200

    ScrollView
    rajout d'un scroll view ainsi que la description de chaque image présentée dans l'activitée d'aide

--- app/src/main/java/brainitall/pixelly/controller/HelpActivity.java
+++ app/src/main/java/brainitall/pixelly/controller/HelpActivity.java
@@ -5,12 +5,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import brainitall.pixelly.R;
 
 public class HelpActivity extends AppCompatActivity {
+
+    /**
+     * élement graphique qui va permettre de faire défiler l'activitée aide
+     */
+    private ScrollView mDefilement;
+
+    /**
+     * mise en page de l'activitée d'aide
+     */
+    private LinearLayout mMiseEnPage;
+
 
     /**
      * textView de l'activitée d'aide
@@ -23,20 +36,21 @@ public class HelpActivity extends AppCompatActivity {
     private ImageView mImage1;
 
     /**
-     * seconde image présentée dans l'activitée aide
+     *  texte de description de la première image présentant le fonctionnement du jeu
      */
-
-    private ImageView mImage2;
-
     private TextView mTexteAide2;
 
-    private Button mBouton;
+    /**
+     * seconde image présentée dans l'activitée aide
+     */
+    private ImageView mImage2;
 
-    private RelativeLayout mRelativeLayout;
-
+    /**
+     * texte decrivant la seconde image présentant le fonctionnement du jeu
+     */
     private TextView mTexteAide3;
 
-    private Button mBouton2;
+
 
 
     @Override
@@ -48,21 +62,13 @@ public class HelpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help);
 
         // branchement de chaque widget de cette activitée
+        mDefilement = (ScrollView) findViewById(R.id.activity_help_defilement); // on réference l'élement graphique qui va servir à faire défiler l'activitée(ScrollView)
+        mMiseEnPage = (LinearLayout) findViewById(R.id.activity_help_mise_en_page);
         mTexteAide = (TextView) findViewById(R.id.activity_help_textview1); // on réference l'élement graphique mTexteAide(TextView)
         mImage1 = (ImageView) findViewById(R.id.activity_help_image1); // on réference l'élément graphique mImage1 (ImageView)
         mImage2= (ImageView) findViewById(R.id.activity_help_image2);
         mTexteAide2 = ( TextView) findViewById(R.id.activity_help_textview2);
-        mBouton = (Button) findViewById(R.id.activity_help_Button1);
-        mRelativeLayout = (RelativeLayout) findViewById(R.id.activity_help_detailLayout);
         mTexteAide3 = (TextView) findViewById(R.id.activity_help_textview3);
-        mBouton2 = (Button) findViewById(R.id.activity_help_Button2);
 
-        mBouton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mRelativeLayout.setVisibility(View.VISIBLE); // le clic sur le bouton, rend le relativeLayout visible
-                mBouton.setVisibility(View.GONE); // le bouton " plus de détail" est masqué
-            }
-        });
     }
 }
