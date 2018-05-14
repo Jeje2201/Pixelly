f01fd6039d2a0107ba572b244fee8a00c9e43f73
lofit13 <38454882+lofit13@users.noreply.github.com>
2018-05-14 10:52:23 +0200

    legere_modif

--- app/src/main/java/brainitall/pixelly/controller/LevelActivity.java
+++ app/src/main/java/brainitall/pixelly/controller/LevelActivity.java
@@ -47,13 +47,14 @@ public class LevelActivity extends AppCompatActivity {
         mNiveau1 = (Button) findViewById(R.id.activity_level_btn_niveau1);
         mNiveau2 = (Button) findViewById(R.id.activity_level_btn_niveau2);
         mHelp = (Button) findViewById(R.id.activity_main_btn_aide);
+        mNiveau1.setEnabled(false); // on désactive le bouton niveau 1
+        mNiveau2.setEnabled(false); // on désactive le bouton niveau 2 qui sera debloqué lors d'un succés au niveau 1
 
         mNiveauTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent playActivity = new Intent(LevelActivity.this, PlayActivity.class);
-                mNiveau1.setEnabled(false); // on désactive le bouton niveau 1
-                mNiveau2.setEnabled(false); // on désactive le bouton niveau 2 qui sera debloqué lors d'un succés au niveau 1
+
                 startActivity(playActivity);
 
                 // ----------------------------il faudra activer les boutons lorsqu'un niveau sera finit -------------------------
