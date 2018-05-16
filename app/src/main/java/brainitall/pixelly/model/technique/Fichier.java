94575d8c79063edf52ea09148d49a223d14b7a60
Yukito13 <38455237+Yukito13@users.noreply.github.com>
2018-05-16 14:40:36 +0200

    normalement c'est bon

--- app/src/main/java/brainitall/pixelly/model/technique/Fichier.java
+++ app/src/main/java/brainitall/pixelly/model/technique/Fichier.java
@@ -1,14 +1,12 @@
 package brainitall.pixelly.model.technique;
 
 import android.content.Context;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-
+import java.io.UnsupportedEncodingException;
 import brainitall.pixelly.controller.Manager;
 
 /**
@@ -44,8 +42,6 @@ public class Fichier {
             is.close();
             json = new String(buffer, "UTF-8");
 
-<<<<<<< HEAD
-
             JSONObject jsonRootObject = new JSONObject(json);
             //Racine de l'objet infoGrille
             JSONArray infoGrille = jsonRootObject.optJSONArray("infoGrille");
@@ -58,20 +54,10 @@ public class Fichier {
 
             for(int i = 0; i < infoGrille.length(); i++){
                 JSONObject jsonObject = infoGrille.getJSONObject(i);
-=======
-        try {
-
-            JSONObject jsonRootObject = new JSONObject(json);
-            JSONArray jsonArray = jsonRootObject.optJSONArray("infoGrille");
-
-            for(int i = 0; i < jsonArray.length(); i++){
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
->>>>>>> 637ee8caf29c991af1d771796ad78b01fb880e9a
                 int numGrille = Integer.parseInt(jsonObject.optString("numGrille").toString());
                 int hauteur = Integer.parseInt(jsonObject.optString("hauteur").toString());
                 int largeur = Integer.parseInt(jsonObject.optString("largeur").toString());
-
-<<<<<<< HEAD
+                // Association de la grille
                 Manager.getInstance().ajouterGrille(numGrille,largeur,hauteur);
             }
 
@@ -81,15 +67,6 @@ public class Fichier {
 
             for (int i = 0; i < terminaison.length(); i++) {
                 JSONObject jsonObject = terminaison.getJSONObject(i);
-=======
-                // Association de la grille
-                Manager.getInstance().ajouterGrille(numGrille,largeur,hauteur);
-            }
-
-            JSONObject  terminaison = jsonRootObject.getJSONObject("terminaison");
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
->>>>>>> 637ee8caf29c991af1d771796ad78b01fb880e9a
                 int tailleChemin = Integer.parseInt(jsonObject.optString("tailleChemin").toString());
                 int x = Integer.parseInt(jsonObject.optString("x").toString());
                 int y = Integer.parseInt(jsonObject.optString("y").toString());
@@ -97,19 +74,17 @@ public class Fichier {
                 int g = Integer.parseInt(jsonObject.optString("g").toString());
                 int b = Integer.parseInt(jsonObject.optString("b").toString());
 
-<<<<<<< HEAD
                 //Ajouter les valeurs dans la methode ajouterTerminaison() de la classe Manager
-=======
-
                 //Ajouter les valeurs dans la grille via le Manager
->>>>>>> 637ee8caf29c991af1d771796ad78b01fb880e9a
                 Manager.getInstance().ajouterTerminaison(tailleChemin,x,y,r,g,b);
             }
 
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         } catch (JSONException e) {
             e.printStackTrace();
-        }catch (IOException ex) {
-            ex.printStackTrace();
         }
     }
 }
