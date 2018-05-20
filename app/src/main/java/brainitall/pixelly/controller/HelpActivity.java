4926f2e6b0062af5b2d36fa32a7b79d280fd1eb8
MrJeje_ <jeremy.leriche22@gmail.com>
2018-05-20 16:04:26 +0200

    Lire description
    + Creation de la page de credit
    ++ Ajout de l'activity dans l'androidManifest.xml
    ++ Creation du fichier CreditActivity.java avec son instentiation
    + Supression d'import non utilisés dans helpactivity.java
    + Suppression des textes des boutons du menu car remplacé par des images

--- app/src/main/java/brainitall/pixelly/controller/HelpActivity.java
+++ app/src/main/java/brainitall/pixelly/controller/HelpActivity.java
@@ -2,11 +2,8 @@ package brainitall.pixelly.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
